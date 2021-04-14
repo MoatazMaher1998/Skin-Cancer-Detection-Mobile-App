a8ed55fe007ec097ad44d8ecff40492cbc6d994b
Moataz Maher <Moatazmaher1998@gmail.com>
2021-04-14 23:42:14 +0200

    Upload Image FrontEnd

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,8 @@ class MyApp extends StatelessWidget {
         child: DefaultTabController(
           length: 3,
           child: Scaffold(
+            backgroundColor: Colors.white,
             appBar: AppBar(
-              leading: Text(
-                "Cancer",
-                style: TextStyle(fontSize: 20),
-              ),
-              leadingWidth: 200,
               backgroundColor: Colors.teal,
               elevation: 50,
               bottom: TabBar(
@@ -36,7 +32,10 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              toolbarHeight: 80,
+              toolbarHeight: 75,
+            ),
+            body: TabBarView(
+              children: [Test1(), Test2(), Test3()],
             ),
           ),
         ),
@@ -44,6 +43,78 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//TabBarView(
-//               children: [],
-//             ),
+
+class Test2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Test3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Test1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 50),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "    Cancer",
+                  style: TextStyle(
+                      fontFamily: 'Zen Dots', fontSize: 20, color: Colors.teal),
+                ),
+              ),
+              Expanded(child: Image.asset("images/logo.png")),
+              Expanded(
+                child: Text(
+                  "    Free",
+                  style: TextStyle(
+                      fontFamily: 'Zen Dots', fontSize: 20, color: Colors.teal),
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 50),
+                    width: 250,
+                    color: Colors.lightGreen,
+                    child: FlatButton(
+                      child: Text(
+                        "UPLOAD",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 250,
+                    height: 200,
+                    child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra vestibulum risus at porttitor. Suspendisse et iaculis quam, ac tempor est. Phasellus condimentum finibus pulvinar. In luctus metus a eleifend pellentesque."),
+                  )
+                ],
+              ),
+              color: Colors.white,
+            ))
+      ],
+    );
+  }
+}
