20504f16f85db70913be3ab26c1095128c549ff9
Moataz Maher <Moatazmaher1998@gmail.com>
2021-04-25 15:19:05 +0200

    Added API without displaying result on UI

--- lib/upload.dart
+++ lib/upload.dart
@@ -1,102 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+class Upload extends StatefulWidget {
+  @override
+  _UploadState createState() => _UploadState();
+}
 
-class Upload extends StatelessWidget {
+class _UploadState extends State<Upload> {
+  File _image;
+  final picker = ImagePicker();
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future upload(File imageFile) async {
+    var stream =
+        new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+    var length = await imageFile.length();
+    var uri = Uri.parse("https://covidapi-alexuni.herokuapp.com/API");
+    var request = new http.MultipartRequest("POST", uri);
+    var multipartFile = new http.MultipartFile('img', stream, length,
+        filename: basename(imageFile.path));
+    request.files.add(multipartFile);
+    var response = await request.send();
+    print(response.statusCode);
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+      return value;
+    });
+  }
+
+  var result;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ListView(
-      children: [
-        SizedBox(height: 50),
-        Row(children: <Widget>[
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "    Cancer",
-                    style: TextStyle(
-                        fontFamily: 'Zen Dots',
-                        fontSize: 20,
-                        color: Colors.teal),
-                  ),
-                ),
-                Expanded(child: Image.asset("images/logo.png")),
-                Expanded(
-                  child: Text(
-                    "    Free",
-                    style: TextStyle(
-                        fontFamily: 'Zen Dots',
-                        fontSize: 20,
-                        color: Colors.teal),
-                  ),
-                )
-              ],
-            ),
-          )
-        ]),
-        Row(children: <Widget>[
-          Expanded(
-              flex: 2,
-              child: Container(
-                child: Column(
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: Icon(Icons.add_a_photo),
+        ),
+        body: Container(
+            child: ListView(
+          children: [
+            SizedBox(height: 50),
+            Row(children: <Widget>[
+              Expanded(
+                child: Row(
                   children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 50),
-                      width: 250,
-                      color: Colors.lightGreen,
-                      child: FlatButton(
-                        onPressed: () async {
-                          var url = Uri.parse(
-                              'https://api.algorithmia.com/v1/algo/moatazmaher/Skintest/1.0.0');
-                          Map<String, String> headers = {
-                            'Content-Type': 'application/json',
-                            'authorization':
-                                'Basic c3R1ZHlkb3RlOnN0dWR5ZG90ZTEyMw=='
-                          };
-                          final test = jsonEncode({
-                            'matrix_a': [
-                              [0, 1],
-                              [2, 0]
-                            ],
-                            'matrix_b': [
-                              [0, 1],
-                              [2, 0]
-                            ]
-                          });
-
-                          var response =
-                              await http.post(url, body: test, headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization':
-                                'Simple sim+fZhO70XsHOvA0OMEnIFu8J91'
-                          });
-                          print('Response status: ${response.statusCode}');
-                          print('Response body: ${response.body}');
-                        },
-                        child: Text(
-                          "UPLOAD",
-                          style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold),
-                        ),
+                    Expanded(
+                      child: Text(
+                        "    Cancer",
+                        style: TextStyle(
+                            fontFamily: 'Zen Dots',
+                            fontSize: 20,
+                            color: Colors.teal),
                       ),
                     ),
-                    Container(
-                      width: 250,
-                      height: 200,
+                    Expanded(child: Image.asset("images/logo.png")),
+                    Expanded(
                       child: Text(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra vestibulum risus at porttitor. Suspendisse et iaculis quam, ac tempor est. Phasellus condimentum finibus pulvinar. In luctus metus a eleifend pellentesque."),
+                        "    Free",
+                        style: TextStyle(
+                            fontFamily: 'Zen Dots',
+                            fontSize: 20,
+                            color: Colors.teal),
+                      ),
                     )
                   ],
                 ),
-                color: Colors.white,
-              ))
-        ])
-      ],
-    ));
+              )
+            ]),
+            Row(children: <Widget>[
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 50),
+                          width: 250,
+                          color: Colors.lightGreen,
+                          child: FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                result = upload(_image);
+                                print("${result.toString()}");
+                              });
+                            },
+                            child: Text(
+                              "UPLOAD",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: 250,
+                            height: 200,
+                            child: _image == null
+                                ? Text('No image selected.')
+                                : Image.file(_image)),
+                        Text("$result")
+                      ],
+                    ),
+                    color: Colors.white,
+                  ))
+            ])
+          ],
+        )),
+      ),
+    );
   }
 }
